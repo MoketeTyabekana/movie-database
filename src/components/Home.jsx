1622f6bf7e968873b283adec5304ad028f7272e4
Mokete <ematyabekana@gmail.com>
2024-12-27 19:29:50 +0200

    centered the search text placeholder and changed the bg color to white

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,11 +17,11 @@ function Home() {
         </div>
         <div className="w-full max-w-2xl space-y-4">
           <div className="relative">
-            <Search className="absolute left-4 top-3.5 h-5 w-5 text-gray-400" />
+            <Search className="absolute left-4 top-3.5 h-5 w-5 text-gray-500" />
             <input
               type="text"
               placeholder="Search for movies"
-              className="w-full px-12 py-3 rounded-full bg-gray-700 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-orange-500"
+              className="w-full px-12 py-3 text-center rounded-full bg-white-500 text-white placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-orange-500"
             />
           </div>
 
