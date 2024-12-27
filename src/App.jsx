72707c44f91fc68e0a97b6e03fa6258948ddb942
Mokete <ematyabekana@gmail.com>
2024-12-27 19:25:48 +0200

    Created a footer component with copyright text, and social media icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import NavBar from './components/NavBar'
-import Home from './components/Home'
+
+import './App.css';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import FooterContainer from './components/FooterContainer';
+
+
+
 
 function App() {
 
@@ -11,6 +13,7 @@ function App() {
     <>
     <NavBar/>
     <Home/>
+   <FooterContainer/>
     </>
   )
 }
---
+++ src/components/FooterContainer.jsx
@@ -0,0 +1,77 @@
+import {
+  FaGithub,
+  FaLinkedin,
+  FaTwitter,
+  FaFacebook,
+  FaInstagram,
+} from "react-icons/fa";
+import React from "react";
+
+export default function FooterContainer() {
+  return (
+    <footer className="bg-black text-white py-8 px-4 mx-auto p-5 border-t-2 border-gray-900">
+      <div className="container mx-auto">
+        <div className="flex flex-col md:flex-row justify-between items-center">
+          <div className="mb-4 md:mb-0 flex gap-5 ">
+            <p>EMTMovies©2024</p>
+            <p className="text-sm">
+              Developed By :{" "}
+              <span className="text-orange-500">Mokete Tyabekana</span>
+            </p>
+          </div>
+
+          <div className="flex-row text-left">
+            <p className="text-sm mb-4">Follow Me</p>
+            <div className="flex space-x-6">
+              <a
+                href="https://linkedin.com"
+                aria-label="LinkedIn"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-orange-400"
+              >
+                <FaLinkedin className="text-lg" />
+              </a>
+              <a
+                href="https://twitter.com"
+                aria-label="Twitter"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-orange-400"
+              >
+                <FaTwitter className="text-lg" />
+              </a>
+              <a
+                href="https://github.com"
+                aria-label="GitHub"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-orange-400"
+              >
+                <FaGithub className="text-lg" />
+              </a>
+              <a
+                href="https://facebook.com"
+                aria-label="Facebook"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-orange-400"
+              >
+                <FaFacebook className="text-lg" />
+              </a>
+              <a
+                href="https://instagram.com"
+                aria-label="Instagram"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-orange-400"
+              >
+                <FaInstagram className="text-lg" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
