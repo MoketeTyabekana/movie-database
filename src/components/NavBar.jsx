cedcf7ed0332f9dfe6aefd695a80f88f269e9177
Mokete <ematyabekana@gmail.com>
2024-12-27 17:03:40 +0200

    Added the logo Icon.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import icon from '../assets/icon.png';
 
 function NavBar() {
   return (
@@ -6,7 +7,8 @@ function NavBar() {
 <nav className="bg-white border-gray-200 dark:bg-black">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-5">
     <a  className="flex items-center space-x-3 rtl:space-x-reverse">
-        <h1 className='text-white '><span className='text-orange-400'>EMT</span>Movies</h1>
+    <img src={icon} alt="Logo" className='w-10'/>
+        <h1 className='text-white font-bold '><span className='text-orange-400'>EMT</span>Movies</h1>
     </a>
     <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
         <span className="sr-only">Open main menu</span>
