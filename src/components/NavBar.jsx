0942b7f5364acb3490404d4826d09f261fdfc31f
Mokete <ematyabekana@gmail.com>
2025-01-07 13:53:26 +0200

    Updated the navbar manually to remove the branch comflics in github.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ import FooterContainer from './FooterContainer';
 function NavBar() {
   return (
     <>
-      <nav className="bg-white dark:bg-black w-screen  fixed top-0 left-0 shadow-md z-50">
+      <nav className="bg-white-400  bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border-b-2 border-gray-400 border-opacity-10 w-screen  fixed top-0 left-0 shadow-md z-50">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-5">
           <Link to="/Home" className="flex items-center space-x-3">
             <img src={icon} alt="Logo" className="w-8" />
@@ -41,11 +41,11 @@ function NavBar() {
             className="hidden w-full md:block md:w-auto"
             id="navbar-default"
           >
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-black md:dark:bg-black dark:border-gray-700">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 md:mt-0 md:border-0">
               <li>
                 <Link
                   to="/"
-                  className="block py-2 px-3 text-white hover:text-orange-400 md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+                  className="block py-2 px-3 text-white bg-blue-700 rounded hover:text-orange-400 md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
                 >
                   Home
                 </Link>
@@ -53,9 +53,9 @@ function NavBar() {
               <li>
                 <Link
                   to="/about"
-                  className="block py-2 px-3 text-white rounded md:bg-transparent hover:text-orange-400 md:text-blue-700 md:p-0 dark:text-white"
+                  className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent hover:text-orange-400 md:text-blue-700 md:p-0 dark:text-white"
                 >
-                  About Me
+                  About Us
                 </Link>
               </li>
             </ul>
