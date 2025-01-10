fe2fda465ace99eea63e29774e525e33c81f2f4d
Mokete <ematyabekana@gmail.com>
2025-01-10 10:39:10 +0200

    changed the text color of the movie description.

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -7,32 +7,34 @@ const MovieDetails = ({ movie, isOpen, onClose}) => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center p-4 z-50">
-      <div className="bg-gray-800 rounded-lg max-w-3xl w-full relative">
+      <div className="bg-gray-900 rounded-lg max-w-3xl w-full relative">
         <button
           onClick={onClose}
-          className="absolute top-4 right-4 text-gray-400 hover:text-white"
+          className="absolute top-6 right-6 text-gray-300 hover:text-orange-400"
         >
           <AiOutlineClose className="w-6 h-6"/>
         </button>
         
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 p-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 items-center  gap-4 p-5">
           <div>
             <img
               src={movie.Poster !== "N/A" ? movie.Poster : ""}
               alt={movie.Title}
-              className="w-full h-[300px] object-cover rounded-lg"
+              className="w-full h-[450px] object-cover rounded-lg"
             />
+            
           </div>
           
           <div>
-            <h2 className="text-2xl font-bold mb-2">{movie.Title}</h2>
-            <p className="text-gray-400 mb-4">{movie.Year} | IMDb: {movie.imdbRating}</p>
+            <h2 className="text-2xl text-orange-400 font-bold mb-2">{movie.Title}</h2>
+            <p className="text-orange-400 mb-4">{movie.Year} | IMDb: {movie.imdbRating}</p>
+            <hr />
             <p className="text-gray-300 mb-4">{movie.Plot}</p>
-            
+            <hr />
             <div className="mb-4">
-              <p className="text-gray-400">Director : {movie.Director}</p>
-              <p className="text-gray-400><span ">Cast : {movie.Actors}</p>
-              <p className="text-gray-400">Genre: {movie.Genre}</p>
+              <p className="text-orange-400">Director : {movie.Director}</p>
+              <p className=" text-orange-400">Cast : {movie.Actors}</p>
+              <p className="text-orange-400">Genre: {movie.Genre}</p>
             </div>
           </div>
         </div>
