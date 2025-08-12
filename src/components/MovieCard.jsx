36aa7ebbfb8466f0f5ff7484b71fbd0d02a45051
Mokete <ematyabekana@gmail.com>
2025-08-12 19:10:58 +0200

    Enhance MovieCard layout with icons and adjust MovieGrid column count

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { BadgeCheck, Calendar, Star } from 'lucide-react';
 
 // movie card compontent taking movie and onclick prop.
 // An image,title,rating,release date inputs that handles the front view of the data from the API.
@@ -14,15 +15,17 @@ const MovieCard = ({ movie, onClick }) => {
         <img
           src={movie.Poster !== "N/A" ? movie.Poster : ""}
           alt={movie.Title}
-          className="w-full h-[250px] object-cover"
+          className="w-full h-auto object-cover"
         />
         
       </div>
-      <div className="p-4">
-        <h3 className="text-sm font-bold truncate mb-1 text-orange-400">{movie.Title}</h3>
+      <div className="p-4 flex flex-col gap-4">
+        <h3 className="text-lg font-bold truncate mb-1 text-orange-400 flex items-center"><BadgeCheck className="text-sm mr-1 w-4 h-4"/>
+
+{movie.Title}</h3>
         <div className="flex justify-between text-xs text-white font-bold">
-         <p> {movie.Year}</p>
-        <p>IMDb:{movie.imdbRating}</p>
+         <p className="text-lg font-bold truncate mb-1 text-white flex items-center"><Calendar className="text-sm mr-1 w-4 h-4  text-orange-400 "/> {movie.Year}</p>
+       <p className="text-lg font-bold truncate mb-1 text-white flex items-center"><Star className="text-sm mr-1 w-4 h-4 text-orange-400"/>{movie.imdbRating}</p>
         </div>
       </div>
     </div>
